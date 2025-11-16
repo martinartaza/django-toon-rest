413995bcd61432c7e077caafedd2de2ab8957a3b
Sebastian Martin, Artaza Saade <sebastian.artaza@acidlabs.com>
2025-11-15 21:52:33 -0300

    Release 0.1.1

---
+++ django_toon_rest/__init__.py
@@ -0,0 +1,11 @@
+"""
+django-toon-rest
+A Django REST Framework renderer for TOON format.
+"""
+
+__version__ = "0.1.1"
+
+from django_toon_rest.renderers import TOONRenderer
+
+__all__ = ['TOONRenderer']
+
